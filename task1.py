2f22b4c3d2684e46f3218bef274daaf6434c6148
theknight666 <93051917+theknight666@users.noreply.github.com>
2025-03-11 01:43:37 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,16 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+#basic math operation
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+
+# Display results
+print("\nResults:")
+print(f"Addition:{addition}")
+print(f"Subtraction:{subtraction}")
+print(f"Multiplication: {multiplication}")
+print(f"Division = {division}")
